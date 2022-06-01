0cea98110aa5349d09502bcf597764c2c2bdb250
Alexander Seiler <seileralex@gmail.com>
2022-06-01 04:02:20 +0200

    Clean up

--- resources/lib/rtsplaytv.py
+++ resources/lib/rtsplaytv.py
@@ -104,7 +104,6 @@ def run():
             'Most_Searched_TV_Shows',
             'Shows_By_Date',
             'Search',
-            # 'Live_TV',
             'RTS_YouTube',
         ]
         RTSPlayTV().build_main_menu(identifiers)
@@ -132,8 +131,6 @@ def run():
         RTSPlayTV().build_date_menu(name)
     elif mode == 25:
         RTSPlayTV().pick_date()
-    # elif mode == 26:
-    #     RTSPlayTV().build_tv_menu()
     elif mode == 27:
         RTSPlayTV().build_search_menu()
     elif mode == 28:
